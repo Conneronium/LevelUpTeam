6813c42336352f13fcb3d2823df3d2119a69ccba
Conner Strazzo <connerstrazzo@gmail.com>
2022-12-04 08:18:59 +0000

    Our first commit

--- linux.sh
+++ linux.sh
@@ -3,6 +3,7 @@
 echo "Please enter your favorite animal:"
 read animal
 echo "Reading.... please wait"
+#i changed name to favorite animal haha!:
 sleep 3 
 echo "-----------------------------"
 echo "Your username is:" && whoami 
@@ -13,10 +14,11 @@ echo "Please wait while we process the date......."
 sleep 3
 echo "Made on:" && date
 echo "-----------------------------"
+#this is a just a comment lol
 sleep 2
 echo "My IP address is:"
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
-
+#I just now added this!
 This is week 4 - project test
